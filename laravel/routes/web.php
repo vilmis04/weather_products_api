e65f7c4a61d3ff8bfa9c89e60a4978c18956876b
Vilmantas Sudaris <vilmantas.sudaris@gmail.com>
2022-05-11 20:24:04 +0300

    init

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/api/products/recommended/{city}', function (string $city) {
     return view('welcome');
 });
