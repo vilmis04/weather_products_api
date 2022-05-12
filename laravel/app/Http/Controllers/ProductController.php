46798d9421e4d6128810acb8f969ecc49780ad92
Vilmantas Sudaris <vilmantas.sudaris@gmail.com>
2022-05-12 22:33:25 +0300

    upd: forcast for next 3 days added

--- laravel/app/Http/Controllers/ProductController.php
+++ laravel/app/Http/Controllers/ProductController.php
@@ -7,8 +7,10 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    public function recommend(string $city): string
+    // public function recommend(string $city): string
+    public function recommend(string $city)
     {
+        $city = htmlspecialchars($city);
         $response = (new Product)->recommendProductByWeather($city);
         return $response;
     }
--- laravel/app/Models/Product.php
+++ laravel/app/Models/Product.php
@@ -12,8 +12,59 @@ class Product extends Model
 
     public function recommendProductByWeather(string $city)
     {
-        $uri = 'https://api.meteo.lt/v1/places/' . $city . '/forecasts/long-term';
-        $response = Http::get($uri);
-        return $response;
+        $url = 'https://api.meteo.lt/v1/places/' . $city . '/forecasts/long-term';
+        $response = json_decode(Http::get($url), true);
+
+        $forecasts = $this->getThreeDayForecast($response['forecastTimestamps']);
+        $mostOccuringWeather = $this->getMostOccuringWeather($forecasts);
+
+        return $mostOccuringWeather;
+    }
+
+    private function getThreeDayForecast(array $forecastTimestamps): array
+    {
+        $forecasts = [];
+        $interval = [
+            (date_create('tomorrow'))->format('Y-m-d'),
+            (date_create('tomorrow + 1 day'))->format('Y-m-d'),
+            (date_create('tomorrow + 2 days'))->format('Y-m-d')
+        ];
+
+        foreach ($forecastTimestamps as $forecast) {
+            $dateAndTime = explode(' ', $forecast['forecastTimeUtc']);
+            $date = array_shift($dateAndTime);
+            
+            if (!in_array($date, $interval)) continue;
+            $condition = $forecast['conditionCode'];
+
+            $forecasts[$date][$condition] = $forecasts[$date][$condition] ?? 0;
+            $forecasts[$date][$condition] += 1;
+
+            // if (isset($forecasts[$date][$condition])) {
+            //     $forecasts[$date][$condition]++;
+            // } else {
+            //     $forecasts[$date][$condition] = 1;
+            // }
+        }
+
+        return $forecasts;
+    }
+
+    private function getMostOccuringWeather(array $forecasts): array
+    {
+        $result = [];
+        foreach ($forecasts as $date => $conditions) {
+            $mostOccuringType = '';
+            $occurence = 0;
+            foreach($conditions as $condition => $count) {
+                if ($count > $occurence) {
+                    $mostOccuringType = $condition;
+                    $occurence = $count;
+                }
+            }
+            $result[$date] = $mostOccuringType;
+        }
+
+        return $result;
     }
 }
