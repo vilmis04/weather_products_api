9e0241deaf8a918fa010c8a581a7834d3e1d0700
Vilmantas Sudaris <vilmantas.sudaris@gmail.com>
2022-05-13 17:21:36 +0300

    feat: created database schema

--- laravel/database/migrations/2022_05_11_173305_create_products_table.php
+++ laravel/database/migrations/2022_05_11_173305_create_products_table.php
@@ -16,6 +16,10 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('name');
+            $table->string('sku');
+            $table->float('price');
+            $table->string('condition_1');
         });
     }
 
